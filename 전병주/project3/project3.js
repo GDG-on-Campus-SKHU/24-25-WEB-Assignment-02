f7f0b0f59739db3a841aec9010254c29d0d7d586
ByeongJu-Jeon <skhu202114128@gmail.com>
2024-10-05 00:22:56 +0900

    mend

--- 전병주/project3/project3.js
+++ 전병주/project3/project3.js
@@ -1,7 +1,8 @@
 for (i = 10; i >= 0; i--) {
+  //i가 10부터 0까지 내려오면서 반복
   if (i % 2 == 1) {
-    console.log(i);
-  } else {
-    continue;
+    // i가 홀수인 경우만 출력
+    let str = i.toString(); // i를 문자열로 변환하기 위해 toString()사용
+    console.log(str); // 출력
   }
 }
