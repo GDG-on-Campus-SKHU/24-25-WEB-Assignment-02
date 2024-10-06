f924947e41cee51e60f3c97dac7e48b9a3226284
ByeongJu-Jeon <skhu202114128@gmail.com>
2024-10-07 08:45:35 +0900

    전병주 과제 수정본

--- 전병주/project3/project3.js
+++ 전병주/project3/project3.js
@@ -1,8 +1,12 @@
+let final = [];
+
 for (i = 10; i >= 0; i--) {
   //i가 10부터 0까지 내려오면서 반복
   if (i % 2 == 1) {
     // i가 홀수인 경우만 출력
     let str = i.toString(); // i를 문자열로 변환하기 위해 toString()사용
-    console.log(str); // 출력
+    final.unshift(str); //str를 배열 final에 이동
   }
 }
+
+console.log(final); //출력
