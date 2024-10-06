ff58eefe18fc2cf601eafe3ad193f2394a346d6b
Park Su Jin <80556843+k-loopy@users.noreply.github.com>
2024-10-07 00:02:37 +0900

    Update task4.js

--- 박수진_과제제출/task4.js
+++ 박수진_과제제출/task4.js
@@ -2,7 +2,7 @@ for (let a=1; a<=6; a++) {
     for (let b=1; b<=6; b++) {
         for (let c=1; c<=6; c++) {
             if (a+b+c===10) {
-                console.log(`(${a}, ${a}, ${a})`);
+                console.log(`(${a}, ${b}, ${c})`);
             }
         }
     }
